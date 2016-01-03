5f108462ab332961a0aa7a234f9096927f7c3d47
JOSEPH BULL <jtbull@uw.edu>
2016-01-03 02:05:21 -0800

    clean up crud

--- python-scripts/elasticsearch-websockets-connector.py
+++ python-scripts/elasticsearch-websockets-connector.py
@@ -1,10 +1,7 @@
-# #!/usr/bin/python3
-# __author__ = "donnydevito"
-# __copyright__   = "Copyright 2015, donnydevito"
-# __license__ = "MIT"
-
-# import websockets, time, sys, json, hashlib, zlib, base64, argparse, asyncio
-# from ws4py.client.threadedclient import WebSocketClient
+#!/usr/bin/python3
+__author__ = "donnydevito"
+__copyright__   = "Copyright 2015, donnydevito"
+__license__ = "MIT"
 
 import websocket, time, datetime, sys, json, hashlib, zlib, base64, json, re, elasticsearch, argparse, uuid, pytz
 
@@ -17,15 +14,10 @@ TIMEZONE = pytz.timezone('UTC')
 # ***** CHANGE THIS TO BE THE URL OF YOUR ELASTICSEARCH SERVER *****
 ELASTICSEARCH_HOST = "http://localhost:9200"
 
-# # Bitfinex API time limit in seconds 
-# API_TIME_LIMIT = 1
-
 # Default index name in elasticsearch to use for the btc_usd market data aggregation
 DEFAULT_INDEX_NAME = "btcwebsockettickerarchive"
 
-
 es = None
-
 
 ## TODO: RESTRUCTURE THIS INTO MORE MEANINGFUL CLASSES
 
@@ -90,27 +82,6 @@ def doit(data):
 		processTickerData(dataItem)
 
 	uniqueIdentifier = uuid.uuid4()
-	# for dataPoint in jsonData: 
-	# 	okcoinDto = {}
-	# 	dataJson = dataPoint["data"]
-	# 	okCoinTimestamp = dataJson["timestamp"] 
-	# 	# print(okCoinTimestamp)
-	# 	# dateRecv = datetime.datetime.fromtimestamp(float(okCoinTimestamp))
-	# 	# print(dateRecv)
-	# 	okcoinDto = {}
-	# 	okcoinDto["uuid"] = uniqueIdentifier
-	# 	# okcoinDto["date"] = dateRecv
-	# 	okCoinDto["timestamp"] = str(okCoinTimestamp)
-	# 	okCoinDto["last_price"] = float(dataJson["last"])
-	# 	okCoinDto["volume"] = float(dataJson["vol"]) 
-	# 	okCoinDto["high"] = float(dataJson["high"])
-	# 	okCoinDto["ask"] = float(dataJson["sell"]) 
-	# 	okCoinDto["low"] = float(dataJson["low"]) 
-	# 	okCoinDto["bid"] = float(dataJson["buy"])
-	# 	print("\t---")
-	# 	print(okcoinDto)
-	# 	print("\t---")
-
 	print("\t---")
 	print("\t---\n")
 	print("*********~*~**~*~*******")
@@ -157,7 +128,6 @@ def processTickerData(item):
 	okCoinDto["bid"] = float(bidPrice)
 	putNewDocumentRequest = es.create(index=DEFAULT_INDEX_NAME, doc_type='okcoin', ignore=[400], id=uniqueId, body=okCoinDto)
 	successful = putNewDocumentRequest["created"]
-	
 	if successful == True: 
 		print("WEBSOCKET ENTRY ADDED TO ES CLUSTER")
 	else: 
@@ -189,101 +159,4 @@ if __name__ == "__main__":
 	ws = websocket.WebSocketApp(OKCOIN_WEBSOCKET_URL, on_message = on_message, on_error = on_error, on_close = on_close)
 	ws.on_open = on_open
 	ws.run_forever()
-
-
-
-# def getArgs(): 
-# 	parser = argparse.ArgumentParser(description='BTC elastic search websockets data collector')
-# 	parser.add_argument('--okcoin_secret', default="zzzzzzz")
-# 	parser.add_argument('--okcoin_key', default="zzzzzzzz")
-# 	args = parser.parse_args()
-# 	return args
-
-# def buildMySign(params, secretKey):
-#     sign = ''
-#     for key in sorted(params.keys()):
-#         sign += key + '=' + str(params[key]) + '&'
-#     return  hashlib.md5((sign+'secret_key='+secretKey).encode("utf-8")).hexdigest().upper()
-
-
-
-# class DummyClient(WebSocketClient):
-# 	def opened(self):
-# 	    print("Connection opened")
-# 	    self.send("{'event':'addChannel','channel':'ok_btcusd_ticker'")
-# 	    yield self.recv()
-# 	def closed(self, code, reason=None):
-# 		print("Closed down", code, reason)
-
-# 	def received_message(self, m):
-# 		print(str(m))
-# 		# if len(m) == 175:
-# 		# 	self.close(reason='Bye bye')
-
-# # def okcoinOnMessage(self, event): 
-# # 	okcoinData = inflate(event)
-# # 	print(okcoinData)
-
-# # def okcoinOnError(self, event): 
-# # 	print(event) 
-
-# # def okcoinOnClose(self, event): 
-# # 	logTime = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-# # 	print("[" + logTime + "]: DISCONNECTING FROM OKCOIN WEBSOCKET")
-
-# # def okcoinOnOpen(self, event): 
-# #     self.send("{'event':'addChannel','channel':'ok_btcusd_ticker'}")
-
-
-# # class OkcoinWebSocket(WebSocket):
-
-# #     def handleMessage(self):
-# #        for client in clients:
-# #           if client != self:
-# #              client.sendMessage(self.address[0] + u' - ' + self.data)
-
-# #     def handleConnected(self):
-# #        print self.address, 'connected'
-# #        for client in clients:
-# #           client.sendMessage(self.address[0] + u' - connected')
-# #        clients.append(self)
-
-# #     def handleClose(self):
-# #        clients.remove(self)
-# #        print self.address, 'closed'
-# #        for client in clients:
-# #           client.sendMessage(self.address[0] + u' - disconnected')
-
-
-# async def okCoinConnectToSocket():
-# 	print("O HAI")
-# 	async with websockets.connect(OKCOIN_WEBSOCKET_URL) as websocket:
-# 		print("the lulz")
-# 		# handshake = websocket.handshake.build_request({'event':'addChannel','channel':'ok_btcusd_ticker'})
-# 		print(websocket.send("{'event':'addChannel','channel':'ok_btcusd_ticker'"))
-# 		websocket.ping()
-# 		websocket.pong()
-# 		print(str(websocket.recv()))
-
-# if __name__ == "__main__": 
-	
-# 	# asyncio.get_event_loop().run_until_complete(okCoinConnectToSocket())
-#     try:
-#         ws = DummyClient(OKCOIN_WEBSOCKET_URL)
-#         ws.connect()
-#         ws.run_forever()
-#     except KeyboardInterrupt:
-#         ws.close()
-# 	# args = getArgs()
-# 	# if args.okcoin_secret != None and args.okcoin_key != None: 
-# 	# 	print("Credentials set for Okcoin...")
-# 	# else: 
-# 	# 	raise IOError("Please enter all credentials for okcoin (see -h for help)") 
-# 	# okcoinApiKey = args.okcoin_key
-# 	# okcoinApiSecret = args.okcoin_secret
-
-# 	# websockets.enableTrace(False) 
-# 	# okcoinWebSocket = websockets.WebSocketApp(OKCOIN_WEBSOCKET_URL, on_message = okcoinOnMessage, on_error = okcoinOnError, on_close = okcoinOnClose) 
-# 	# okcoinWebSocket.on_open = okcoinOnOpen
-# 	# okcoinWebSocket.run_forever()
 
